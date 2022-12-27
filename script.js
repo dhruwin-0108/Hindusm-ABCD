dcdd09816c1086a46ac1f8783bb4206b6882a693
dhruwin-0108 <dhruvkapadiya660@gmail.com>
2022-12-27 12:09:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,181 @@
+function toggle1() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop1 = document.getElementById("popup1");
+  pop1.classList.toggle("active");
+}
+
+function toggle2() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop2 = document.getElementById("popup2");
+  pop2.classList.toggle("active");
+}
+
+function toggle3() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop3 = document.getElementById("popup3");
+  pop3.classList.toggle("active");
+}
+
+function toggle4() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop4 = document.getElementById("popup4");
+  pop4.classList.toggle("active");
+}
+
+function toggle5() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop5 = document.getElementById("popup5");
+  pop5.classList.toggle("active");
+}
+
+function toggle6() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop6 = document.getElementById("popup6");
+  pop6.classList.toggle("active");
+}
+
+function toggle7() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop7 = document.getElementById("popup7");
+  pop7.classList.toggle("active");
+}
+
+function toggle8() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop8 = document.getElementById("popup8");
+  pop8.classList.toggle("active");
+}
+
+function toggle9() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop9 = document.getElementById("popup9");
+  pop9.classList.toggle("active");
+}
+
+function toggle10() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop10 = document.getElementById("popup10");
+  pop10.classList.toggle("active");
+}
+
+function toggle11() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop11 = document.getElementById("popup11");
+  pop11.classList.toggle("active");
+}
+
+function toggle12() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop12 = document.getElementById("popup12");
+  pop12.classList.toggle("active");
+}
+
+function toggle13() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop13 = document.getElementById("popup13");
+  pop13.classList.toggle("active");
+}
+
+function toggle14() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop14 = document.getElementById("popup14");
+  pop14.classList.toggle("active");
+}
+
+function toggle15() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop15 = document.getElementById("popup15");
+  pop15.classList.toggle("active");
+}
+
+function toggle16() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop16 = document.getElementById("popup16");
+  pop16.classList.toggle("active");
+}
+
+function toggle17() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop17 = document.getElementById("popup17");
+  pop17.classList.toggle("active");
+}
+
+function toggle18() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop18 = document.getElementById("popup18");
+  pop18.classList.toggle("active");
+}
+
+function toggle19() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop19 = document.getElementById("popup19");
+  pop19.classList.toggle("active");
+}
+
+function toggle20() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop20 = document.getElementById("popup20");
+  pop20.classList.toggle("active");
+}
+
+function toggle21() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop21 = document.getElementById("popup21");
+  pop21.classList.toggle("active");
+}
+
+function toggle22() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop22 = document.getElementById("popup22");
+  pop22.classList.toggle("active");
+}
+
+function toggle23() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop23 = document.getElementById("popup23");
+  pop23.classList.toggle("active");
+}
+
+function toggle24() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop24 = document.getElementById("popup24");
+  pop24.classList.toggle("active");
+}
+
+function toggle25() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop25 = document.getElementById("popup25");
+  pop25.classList.toggle("active");
+}
+
+function toggle26() {
+  let block = document.getElementById("body");
+  block.classList.toggle("none");
+  let pop26 = document.getElementById("popup26");
+  pop26.classList.toggle("active");
+}
